da68ba7386458bb364cdacd463ee6e0b842f84e4
Himanshu Dahiya <daddy@Himanshus-MacBook-Air.local>
2026-05-08 00:18:07 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,19 @@ def main():
 
         if user_command =="exit":
             break
-        sys.stdout.write(f"{user_command}: command not found\n")
+
+        user_instruction = user_command.split()
+
+        command  = user_instruction[0]
+
+        if command == "echo":
+            sys.stdout.write(f"{' '.join(user_instruction[1:])}\n")
+        
+
+        if user_command.startswith("echo "):
+            sys.stdout.write(f"{user_command[5:]}\n")
+        else:
+            sys.stdout.write(f"{user_command}: command not found\n")
 
 
 
