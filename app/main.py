a5fb077e4ba7af71c69d72ac9d7811c92cf5d412
Himanshu Dahiya <daddy@Himanshus-MacBook-Air.local>
2026-05-07 23:05:49 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
+    sys.stdout.write("$ ")
+    # print("$ ", end="")
+
 
 
 if __name__ == "__main__":
