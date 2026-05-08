6968472d584da6d62e6b1cd0da11983a51266529
Himanshu Dahiya <daddy@Himanshus-MacBook-Air.local>
2026-05-08 07:25:38 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import sys
 
 
 def main():
+
+    buildin_commands = ["echo" , "exit" , "type"]
     
     while True:
         sys.stdout.write("$ ")
@@ -11,6 +13,7 @@ def main():
         if user_command =="exit":
             break
 
+
         user_instruction = user_command.split()
 
         command  = user_instruction[0]
@@ -18,6 +21,13 @@ def main():
         if command == "echo":
             sys.stdout.write(f"{' '.join(user_instruction[1:])}\n")
         
+        elif command == "type":
+            if len(user_instruction) < 2:
+                sys.stdout.write(f"{user_command}: command not found\n")
+            elif user_instruction[1] in buildin_commands:
+                sys.stdout.write(f"{user_instruction[1]} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{user_command}: command not found\n")
         else:
             sys.stdout.write(f"{user_command}: command not found\n")
 
