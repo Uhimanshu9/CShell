2b08e121eb97ee28f1eeac4d439aea64495a2ed5
Himanshu Dahiya <daddy@Himanshus-MacBook-Air.local>
2026-05-09 17:39:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
+import os
 
+path = os.environ["PATH"].split(":")
 
 def handle_exit(args):
     sys.exit(0)
@@ -9,19 +11,43 @@ def handle_echo(args):
     print(" ".join(args))
 
 
+import os
+
+
 def handle_type(args):
 
+    # No command provided
     if not args:
         print("type: missing argument")
         return
 
     command_name = args[0]
 
+    # Step 1: Check builtin commands
     if command_name in commands:
         print(f"{command_name} is a shell builtin")
-    else:
-        print(f"{command_name}: not found")
+        return
 
+    # Step 2: Search PATH directories
+    paths = os.environ["PATH"].split(":")
+
+    for path in paths:
+
+        # Create full path like:
+        # /usr/bin/ls
+        full_path = os.path.join(path, command_name)
+
+        # Step 3:
+        # Check:
+        # 1. file exists
+        # 2. file is executable
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+
+            print(f"{command_name} is {full_path}")
+            return
+
+    # Step 4: Not found anywhere
+    print(f"{command_name}: not found")
 
 commands = {
     "exit": handle_exit,
