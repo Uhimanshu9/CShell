6d4f86b6a6c18a196d278debec2aca3fe35be7ba
Himanshu Dahiya <daddy@Himanshus-MacBook-Air.local>
2026-05-23 18:49:55 +0530

    codecrafters submit [skip ci]

--- app/jobs.py
+++ app/jobs.py
@@ -1,4 +1,14 @@
+import subprocess
+
+
+
+background_jobs = []
 
 
 def handle_jobs(args):
-    pass
+    if len(args) > 1:
+        if args[-1] == "&":
+            process = subprocess.Popen(args[:-1])
+            background_jobs.append(process)
+
+            print(f"[{len(background_jobs)}] {process.pid}")
